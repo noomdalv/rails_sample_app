237bf97983f2b725116a12a856d96311c0926a06
noomdalv <vladlunasan@gmail.com>
2019-11-14 02:13:42 +0000

    added user resources, show users view and gravatar images

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  #IP whitelist
+  config.web_console.whitelisted_ips = '191.89.201.54'
 end
